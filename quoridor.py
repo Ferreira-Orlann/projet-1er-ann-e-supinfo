03021f48e6a24ba694f5aba022a7013ae3b4d12f
Ferreira-Orlann <ferreira.orlann@gmail.com>
2023-02-24 00:31:04 +0100

    Finish Config Scene + Other
    Finition de la scène + ajout d'autres fichier pour la suite

--- quoridor.py
+++ quoridor.py
@@ -8,12 +8,6 @@ class Quoridor():
         self.__screen = pygame.display.set_mode(size=(1160, 920))
         self.__active_scene = SceneStart(self.__screen, self)
         
-        conf = list()
-        conf.append(2)
-        conf.append(5)
-        conf.append(4)
-        self.__config = conf
-        
         self.Run()
         pass
     
@@ -33,12 +27,6 @@ class Quoridor():
             self.__active_scene.Render(self.__screen)
             self.__active_scene = self.__active_scene.Next()
             pygame.display.flip()
-            
-    def GetConfig(self):
-        # 0 : Nombre de joueurs
-        # 1: Taille du plateau
-        # 2: Nombre de barrières
-        return self.__config
 
 if __name__ == '__main__':
     QUORIDOR = Quoridor()
