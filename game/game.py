7ba0c6c911d496388606a4a2a777f901290450ec
Ferreira-Orlann <ferreira.orlann@gmail.com>
2023-02-27 16:28:42 +0100

    Ajours

--- game/game.py
+++ game/game.py
@@ -1,4 +1,5 @@
 from game.humanplayer import HumanPlayer
+import operator
 
 class Game():
     def __init__(self, quoridor, config, scene):
@@ -11,7 +12,6 @@ class Game():
         self.__barrers = []
         lenboard = config[1]
         for i in range(1,lenboard*2):
-            print(i)
             if (i % 2 == 0):
                 self.__barrers.append([None]*lenboard)
             else:
@@ -20,6 +20,23 @@ class Game():
         # Ajout des joueurs
         self.__players = []
         for i in range(config[0]):
-            self.__players.append(HumanPlayer())
+            self.__players.append(HumanPlayer(self))
             
+    def CheckPath(self, player):
+        pass
+    
+    def GetMoves(self, player):
+        quoridor = self.__quoridor
+        ppos = player.GetPos()
+        possibles_moves = []
+        for i in [quoridor.MOVE_TYPE_UP, quoridor.MOVE_TYPE_DOWN, quoridor.MOVE_TYPE_LEFT, quoridor.MOVE_TYPE_RIGHT]:
+            nextpos = tuple(map(operator.sub, player.GetPos(), i))
+            if nextpos[0] < 0 or nextpos[1] < 0:
+                continue
+            if self.CheckMove():
+                possibles_moves.append(nextpos)
+        return possibles_moves
+    
+    def CheckMove(pos, nextpos):
         
+        pass
