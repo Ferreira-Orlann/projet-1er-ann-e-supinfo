1a07506b662b4387dc9ff3703c48ceb29f3ad9d1
TommyBRISSET <118806752+TommyBRISSET@users.noreply.github.com>
2023-03-10 13:53:58 +0100

    white gamescene

--- projet/render/scene/gamescene.py
+++ projet/render/scene/gamescene.py
@@ -1,9 +1,10 @@
 from render.scene.basescene import BaseScene
+from game.game import Game
 from utils import CheckJson
 
 class GameScene(BaseScene):
     def __init__(self, quoridor, game):
-        super().__init__(quoridor, "configs/gamescene/custom.json", "./assets/page2/background3.jpg")
+        super().__init__(quoridor, "configs/gamescene/custom.json")
         pass
     
     def LoadCustomConfig(self, json):
